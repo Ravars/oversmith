55e9e3a7c6716eaf1e107bde884a9f45ff929f21
Rodrigo Nitzsche <rodnit7@gmail.com>
2023-02-25 20:27:18 -0300

    Player Input e Teste com UI

---
+++ Assets/_Developers/Rodrigo/UIInputSystemTest.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIInpuSystemTest : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        if (player != null)
+        {
+            PlayerInput _playerInput = player.GetComponent<PlayerInput>();
+            _playerInput.actions["UIExit"].performed += OnUIExit;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (player != null)
+        {
+            PlayerInput _playerInput = player.GetComponent<PlayerInput>();
+            _playerInput.actions["UIExit"].performed -= OnUIExit;
+        }
+    }
+
+    void OnUIExit(InputAction.CallbackContext ctx)
+    {
+        PlayerInput _playerInput = player.GetComponent<PlayerInput>();
+        _playerInput.SwitchCurrentActionMap("Player");
+        menu.SetActive(false);
+    }
+}
