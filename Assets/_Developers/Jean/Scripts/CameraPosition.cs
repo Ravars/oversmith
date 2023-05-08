4789140829b3e5376861f6b15a986b10b7a3b494
Jean Miller <81575065+jjeanbatista@users.noreply.github.com>
2023-05-08 10:08:08 -0300

    Menu update

---
+++ Assets/_Developers/Jean/Scripts/CameraPosition.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    
+    public float CameraSpeed = 5f
+
+    public Transform Position1
+    public Transform Position2
+    public transform Position3
+    public Transform Position4
+    
+    private int AtualPosition
+
+
+
+    void Start()
+    {
+        AtualPosition = 1
+        
+        
+        Position1 get transform.localposition = transform.localPosition;
+        if (Position1 != Position2)
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
