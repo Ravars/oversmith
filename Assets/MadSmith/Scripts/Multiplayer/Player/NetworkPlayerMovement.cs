bdad08c1c8dff1055a5ad9801022618be79c2156
Vitor Soares Silva <vitrola.vitor@gmail.com>
2023-10-24 11:43:51 -0300

    teste bug

--- Assets/MadSmith/Scripts/Multiplayer/Player/NetworkPlayerMovement.cs
+++ Assets/MadSmith/Scripts/Multiplayer/Player/NetworkPlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using MadSmith.Scripts.Input;
 using Mirror;
 using UnityEngine;
@@ -6,10 +7,15 @@ namespace MadSmith.Scripts.Multiplayer.Player
 {
     public class NetworkPlayerMovement : NetworkBehaviour
     {
+        public float smoothing = 5f;
+        [SerializeField] private float moveSpeed = 10f;
         [SerializeField] private InputReader inputReader;
         private Vector2 previousInput;
         [SerializeField] private float movementSpeed = 5f;
         [SerializeField] private CharacterController controller = null;
+        private Quaternion _targetRotation;
+        private static readonly int Run = Animator.StringToHash("Run");
+        [SerializeField] private Animator _animator; // temporary
         public override void OnStartAuthority()
         {
             Debug.Log("OnStartAuthority");
@@ -31,8 +37,21 @@ namespace MadSmith.Scripts.Multiplayer.Player
             inputReader.EnableGameplayInput();
             inputReader.MoveEvent += SetMovement;
             inputReader.MoveCanceledEvent += ResetMovement;
+            inputReader.DashEvent += DashOnPerformed;
         }
-        
+
+        private void OnDisable()
+        {
+            inputReader.MoveEvent -= SetMovement;
+            inputReader.MoveCanceledEvent -= ResetMovement;
+            inputReader.DashEvent -= DashOnPerformed;
+        }
+
+        private void DashOnPerformed()
+        {
+            
+        }
+
         [ClientCallback]
         private void Update() => Move();
 
@@ -45,23 +64,36 @@ namespace MadSmith.Scripts.Multiplayer.Player
         [Client]
         private void Move()
         {
-            Vector3 right = controller.transform.right;
-            Vector3 forward = controller.transform.forward;
+            var transform1 = controller.transform;
+            Vector3 right = transform1.right;
+            Vector3 forward = transform1.forward;
             right.y = 0f;
             forward.y = 0f;
-
-            Vector3 movement = right.normalized * previousInput.x + forward.normalized * previousInput.y;
-
-            controller.Move(movement * (movementSpeed * Time.deltaTime));
-        }
-        public void UpdateCountdown(int value)
-        {
-            RpcUpdateCountdown(value);
-        }
-
-        public void RpcUpdateCountdown(int value)
-        {
             
+            Vector3 movementDirection = new Vector3(previousInput.x, 0, previousInput.y);
+            if (movementDirection.magnitude > 0.01f)
+            {
+                _targetRotation = Quaternion.LookRotation(movementDirection);
+            }
+            transform.rotation = Quaternion.Slerp(transform.rotation, _targetRotation, Time.deltaTime * smoothing);
+            movementDirection.Normalize();
+        
+            controller.SimpleMove(movementDirection * moveSpeed);
+            _animator.SetBool(Run,movementDirection.magnitude > 0f);
+            
+
+            // Vector3 movement = right.normalized * previousInput.x + forward.normalized * previousInput.y;
+            //
+            // controller.Move(movement * (movementSpeed * Time.deltaTime));
         }
+        // public void UpdateCountdown(int value)
+        // {
+        //     RpcUpdateCountdown(value);
+        // }
+        //
+        // public void RpcUpdateCountdown(int value)
+        // {
+        //     
+        // }
     }
 }
